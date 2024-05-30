2a08019f29b32a0ca31c34387753310e67dd9ca5
Sen <65850511+Senny10@users.noreply.github.com>
2024-05-30 17:50:04 +0100

    chore: Update next.config.mjs with output, trailingSlash, and basePath options

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: "export",
-    trailingSlash: true,
+	output: "export",
+	trailingSlash: true,
+	basePath: process.env.CI ? "/portfolio" : "",
 };
 
 export default nextConfig;
